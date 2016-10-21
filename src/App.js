773dfa10db08da7750746fb8f9d475733030ca3e
Emil Mieilica <emil.mieilica@gmail.com>
2016-10-22 01:04:54 +0200

    Save local changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state={token:''};
+    }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload
+                    <input
+                        ref="newField"
+                        className="new-todo"
+                        placeholder="Enter the encoded mambo jumbo"
+                        onChange={e => this.handleChange(e)}
+                        value={this.state.token}
+                        autoFocus={true}
+                    />
+                </p>
+            </div>
+        );
+    }
+
+    handleChange(e) {
+        this.setState({token: event.target.value});
+    }
 }
 
 export default App;
